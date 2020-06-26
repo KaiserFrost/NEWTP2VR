25b69384a7cd4f0825fe7a7056448cda4b2000f1
KaiserFrost <brunossj5@hotmail.com>
2020-06-26 17:56:30 +0100

    Update httpf.py

--- http-image/httpserv/httpf.py
+++ http-image/httpserv/httpf.py
@@ -18,7 +18,7 @@ class HttpRequestHandler(http.server.SimpleHTTPRequestHandler):
                     #alterado
                     #http://0.0.0.0:5001
                     
-                    self.send_header('Location','http://192.168.122.2:5001/login?Referer=http://0.0.0.0:'+str(PORT))
+                    self.send_header('Location','http://192.168.122.2:5001/login?Referer=http://192.168.122.111:'+str(PORT))
                     self.end_headers()
     def do_GET(self):
 
